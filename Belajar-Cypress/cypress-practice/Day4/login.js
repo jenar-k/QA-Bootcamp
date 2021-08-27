f8c5a8e9d826864ac34754046ca7f42118fd1fa3
Jenar K <86145540+jenar-k@users.noreply.github.com>
2021-08-27 17:14:49 +0700

    update assert di pembelian.js
    update assert di pembelian.js

--- Belajar-Cypress/cypress-practice/Day4/login.js
+++ Belajar-Cypress/cypress-practice/Day4/login.js
@@ -1,14 +1,38 @@
-describe ('Login', function() {
-    it ('TC-01 Login', function (){
-        cy.visit('https://www.demoblaze.com/index.html');
+describe ('Test', function() {
+    it ('Test', function (){
+        cy.server();
+        cy.route('GET', '/entries').as('loadProducts')
+        cy.aksesHalaman();
+        cy.url().should('include', 'www.demoblaze.com');
+        cy.wait('@loadProducts').its('status').should('eq', 200);
         cy.get('#login2').click();
-        cy.wait(3000);
-        cy.get('#logInModalLabel').should('have.text', 'Log in');
-        cy.get('#loginusername').type('arjen9756').should('have.value', 'arjen9756');
-        cy.wait(3000);
+        cy.get('#logInModal > .modal-dialog > .modal-content > .modal-body').should('be.visible');
+        cy.wait(3000)
+        cy.get('#loginusername').type('Dojo-arjen1324').should('have.value', 'Dojo-arjen1324');
+        cy.wait(3000)
         cy.get('#loginpassword').type('12345678').should('have.value', '12345678');
-        cy.wait(3000);
+        cy.wait(3000)
         cy.get('#logInModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click();
-        cy.get('#nameofuser').should('be.visible');
+        cy.wait(3000);
+        cy.get('.active > .nav-link').should('be.visible');
+        cy.wait(3000);
+        cy.get('#logout2').click();
+        cy.wait(3000);
+        cy.get('.active > .nav-link').should('be.visible');
     })
 })
+
+
+/* describe ('Test', function() {
+    it ('Test', function (){
+        cy.server();
+        cy.route('GET', '/entries').as('loadProducts')
+        cy.aksesHalaman();
+        cy.url().should('include', 'www.demoblaze.com');
+        cy.wait('@loadProducts').its('status').should('eq', 200);
+        cy.get('#login2').click();
+        cy.wait(5000)
+        cy.get('#loginusername').type('Dojo-arjen1324');
+        cy.wait(5000)
+    })
+}) */
--- Belajar-Cypress/cypress-practice/Day4/pembelian.js
+++ Belajar-Cypress/cypress-practice/Day4/pembelian.js
@@ -1,27 +1,34 @@
 describe('Beli', function () {
     it('TC-01 Beli', function () {
-        cy.visit('https://www.demoblaze.com/index.html')
-        // cy.get(':nth-child(2) > .card > .card-block > .card-title > .hrefch').click();
-        cy.contains('Samsung galaxy s6').click();
-        cy.wait(3000);
+        cy.server();
+        cy.route('GET', '/entries').as('loadProducts')
+        cy.aksesHalaman();
+        cy.url().should('include', 'www.demoblaze.com');
+        cy.wait('@loadProducts').its('status').should('eq', 200);        
+        cy.get(':nth-child(1) > .card > .card-block > .card-title > .hrefch').click();
+        cy.route('GET', '/index.m3u8').as('loadDetail')
+        cy.wait('@loadDetail').its('status').should('eq', 200);
         cy.get('.col-sm-12 > .btn').click();
-        cy.wait(3000);
         cy.get('#cartur').click();
         cy.wait(3000);
         cy.get('.col-lg-1 > .btn').click();
         cy.wait(3000);
-        cy.get('#name').type('arjen9756');
+        cy.get('#name').type('Dojo-arjen1324').should('have.value', 'Dojo-arjen1324');
         cy.wait(3000);
-        cy.get('#country').type('Indonesia');
+        cy.get('#country').type('Indonesia').should('have.value', 'Indonesia');
         cy.wait(3000);
-        cy.get('#city').type('Yogyakarta');
+        cy.get('#city').type('Yogyakarta').should('have.value', 'Yogyakarta');
         cy.wait(3000);
-        cy.get('#card').type('121323612813613');
+        cy.get('#card').type('121323612813613').should('have.value', '121323612813613');
         cy.wait(3000);
-        cy.get('#month').type('August');
+        cy.get('#month').type('August').should('have.value', 'August');
         cy.wait(3000);
-        cy.get('#year').type('2022');
+        cy.get('#year').type('2022').should('have.value', '2022');;
         cy.wait(3000);
         cy.get('#orderModal > .modal-dialog > .modal-content > .modal-footer > .btn-primary').click();
+        cy.get('.sweet-alert > h2').should('have.text', 'Thank you for your purchase!');
+        cy.wait(3000);
+        cy.get('.confirm').click();
+        cy.get('.active > .nav-link').should('be.visible');
     })
 })
