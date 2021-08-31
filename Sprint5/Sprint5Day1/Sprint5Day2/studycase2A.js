2074e9f13f84357d34f3551ae1197b30414f8665
Jenar K <86145540+jenar-k@users.noreply.github.com>
2021-08-31 21:46:46 +0700

    update date picker.
    update date picker.

--- Sprint5/Sprint5Day1/Sprint5Day2/studycase2A.js
+++ Sprint5/Sprint5Day1/Sprint5Day2/studycase2A.js
@@ -7,25 +7,20 @@ describe('Admin PKH', function () {
         cy.get('#userEmail').type('jenar@email.com').should('have.value', 'jenar@email.com');
         cy.get('#genterWrapper > .col-md-9 > :nth-child(1) > .custom-control-label').click()
         cy.get('#userNumber').type('080989999').should('have.value', '080989999');
-
-        // cy.get('#dateOfBirthInput').click();
-        // cy.get('.react-datepicker__month-select').select('January');
-        // cy.get('.react-datepicker__year-select').select('1998');
-        // cy.get('class=[react-datepicker__day react-datepicker__day--013]').click();
-
-        // cy.get('.subjects-auto-complete__value-container').type('daftar',{force:true}).should('have.value', 'daftar');
-        // cy.get('.subjects-auto-complete__input').type('daftar').should('have.value', 'daftar');
-        cy.get('.subjects-auto-complete__input',{force:true}).type('daftar').should('have.value', '');
+        cy.get('#dateOfBirthInput').should('have.value', '31 Aug 2021');
+        cy.get('#dateOfBirthInput' , {force : true}).click().type('{selectall}' , '{backspace}').type('13 Jan 2000').type('{enter}');
+        cy.get('#dateOfBirthInput').should('have.value', '13 Jan 2000');
+        cy.get('.subjects-auto-complete__value-container',{force:true}).type('daftar').should('have.value', '');
         cy.get('#hobbiesWrapper > .col-md-9 > :nth-child(1) > .custom-control-label').click();
         cy.get('#hobbiesWrapper > .col-md-9 > :nth-child(2) > .custom-control-label').click();
-
         cy.get('#uploadPicture').attachFile('testgambar.jpg');
-
         cy.get('#currentAddress').type('Ngaglik, Yogyakarta').should('have.value', 'Ngaglik, Yogyakarta');
         cy.get('#state > .css-yk16xz-control > .css-1hwfws3 > .css-1wa3eu0-placeholder').click();
         cy.get('#react-select-3-option-2').click();
         cy.get('#city > .css-yk16xz-control > .css-1hwfws3').click();
         cy.get('#react-select-4-option-0').click();
         cy.get('#submit').click();
+        cy.get('#example-modal-sizes-title-lg').should('have.text', 'Thanks for submitting the form');
+        cy.get('tbody > :nth-child(10) > :nth-child(2)').should('have.text', 'Haryana Karnal')
     })
 })
